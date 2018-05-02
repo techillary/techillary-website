ba871ceb56ed51d5f47abab717f5f466998f036a
iamoperand <narora200@gmail.com>
2018-05-02 18:39:03 +0530

    adding google analytics

--- js/gtag.js
+++ js/gtag.js
@@ -1,8 +1,6 @@
 // Global site tag (gtag.js) - Google Analytics
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments)}
+gtag('js', new Date());
 
-  gtag('config', 'UA-118590356-1');
-</script>
+gtag('config', 'UA-118590356-1');
